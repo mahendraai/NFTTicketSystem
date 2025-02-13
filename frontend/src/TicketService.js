2e88f66c40b7bd2b78757c51c2e76473afb5e20a
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 21:02:47 +0530

    Update TicketService.js

--- frontend/src/TicketService.js
+++ frontend/src/TicketService.js
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+const API_URL = "http://localhost:5000/api/tickets";
+
+const mintTicket = async (userAddress, eventId, metadataUri) => {
+  const response = await axios.post(`${API_URL}/mint`, {
+    userAddress,
+    eventId,
+    metadataUri,
+  });
+  return response.data;
+};
+
+export default { mintTicket };
