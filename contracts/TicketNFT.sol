fe272024420e3ecb8cb073117f927cb6f40de0ac
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 21:05:46 +0530

    Update TicketNFT.sol

--- contracts/TicketNFT.sol
+++ contracts/TicketNFT.sol
@@ -1,1 +1,19 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TicketNFT is ERC721URIStorage, Ownable {
+    uint public ticketCounter;
+
+    constructor() ERC721("TicketNFT", "TNFT") {}
+
+    function mintTicket(address to, uint eventId, string memory metadataUri) public onlyOwner {
+        ticketCounter++;
+        uint ticketId = ticketCounter;
+        _safeMint(to, ticketId);
+        _setTokenURI(ticketId, metadataUri);
+    }
+}
