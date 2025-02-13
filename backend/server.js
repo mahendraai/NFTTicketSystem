a9a2c77b79c9ad839cadc9f6c58e14b094304dd8
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 20:58:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ticketRoutes = require("./routes/ticketRoutes");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use("/api/tickets", ticketRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
