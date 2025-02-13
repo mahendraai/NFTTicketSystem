4f1a4d93d6af167214a4f6bf8f43f00a7f789f36
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 21:01:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,14 @@
+import React, { useState } from "react";
+import TicketMintingForm from "./components/TicketMintingForm";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="App">
+      <h1>NFT Ticketing System</h1>
+      <TicketMintingForm />
+    </div>
+  );
+}
+
+export default App;
