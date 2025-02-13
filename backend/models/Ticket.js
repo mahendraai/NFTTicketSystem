807a9a8c9d8f90b1e4ea8f07b82ae18f53dcb980
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 20:57:35 +0530

    Update Ticket.js

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -1,1 +1,34 @@
 
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Ticket = sequelize.define("Ticket", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  eventId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  tokenId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  metadataUri: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  transactionHash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Ticket;
