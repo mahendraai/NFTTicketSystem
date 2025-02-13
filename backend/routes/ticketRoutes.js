7cf367fced2061b47dcf195cfd0c84852cf27dae
Mahendra Ribadiya <mahendratek@gmail.com>
2025-02-13 20:58:51 +0530

    Update ticketRoutes.js

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { mintTicket } = require("../controllers/ticketController");
 
+router.post("/mint", mintTicket);
+
+module.exports = router;
